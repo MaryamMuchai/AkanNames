e5a261464ccf780ffd3504f31257a9ed6f63f50a
Marya <maryammuchai@gmail.com>
2021-02-21 23:08:12 +0300

    Adding the elseif command on the male gender

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
-let outputTag = document.getElementById ('output')
-let name = document.getElementById('name').nodeValue
-let message = "WELCOME"
-function getName(event) {
-    event.preventDefault()
-    outputTag.innerHTML = message
-    name=document.getElementById('name').nodeValue
-    console.log(name);
-}
-let date=document.getElementById('date').nodeValue
-let message = '${date}'
-let mybirthday=new Date('date').toDateString()
-let day = mybirthday.split(' ')
+let outputTag = document.getElementById('output')
+let name = document.getElementById(;name).value
+
+let myBirthday = new Date(date).toDateString()
+let day = myBirthday.split()
+let name = document.getElementById('name').value
+let gender = document.getElementById('gender').value
+let message = "Here goes your details, date ${date} and this is you ${gender}"
+
+let malenames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+let femalenames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+if (gender==='male && day ==='Sun'){
+   akaname = malenames[0]
+}else if (gender ==='male' $$ day ==='')
